90849fe305baeb46da757bb059681cb40965ac36
liurongqing <liurongqing@zhihuiqiche.com>
2023-07-05 16:04:53 +0800

    feat: init

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import { useEffect } from "react";
+import { invoke } from "@tauri-apps/api/tauri";
+
+export default function Home() {
+  useEffect(() => {
+    invoke<string>("greet", { name: "Next.js" })
+      .then(console.log)
+      .catch(console.error);
+  }, []);
+  return <div>123</div>;
+}
