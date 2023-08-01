dfa33fcef0afb66e72caddd88361be3d2fb6356e
liurongqing <liurongqing@zhihuiqiche.com>
2023-08-01 18:59:56 +0800

    feat: 快速提交

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 import { invoke } from "@tauri-apps/api/tauri";
 import { InboxOutlined } from "@ant-design/icons";
 import { Button, Card, Form, Input, Progress, Upload } from "antd";
@@ -8,9 +8,43 @@ import { Button, Card, Form, Input, Progress, Upload } from "antd";
 const { Dragger } = Upload;
 
 export default function Home() {
+  const formRef = useRef(null);
+  const [fileList, setFileList] = useState<any>([]);
+
+  useEffect(() => {
+    invoke<string>("greet", { name: "Next.js001" })
+      .then(console.log)
+      .catch(console.error);
+  }, []);
+
+  const uploadProps = {
+    onRemove: (file: any) => {
+      const index = fileList.indexOf(file);
+      const newFileList = fileList.slice();
+      newFileList.splice(index, 1);
+      setFileList(newFileList);
+    },
+    beforeUpload(file: any) {
+      console.log("file", file);
+      setFileList([...fileList, file]);
+      return false;
+    },
+    fileList,
+  };
+
+  function handleExport() {
+    // const formData = new FormData();
+    // fileList.forEach((file: any) => {
+    //   formData.append("files[]", file);
+    // });
+    invoke("image_crop", { name: "Next.js001002" })
+      .then(console.log)
+      .catch(console.error);
+  }
+
   return (
     <div className="p-2">
-      <Dragger>
+      <Dragger {...uploadProps}>
         <p className="ant-upload-drag-icon">
           <InboxOutlined />
         </p>
@@ -36,7 +70,7 @@ export default function Home() {
 
       <Progress className="mt-4" percent={50} status="active" />
 
-      <Button type="primary" block>
+      <Button onClick={handleExport} type="primary" block>
         导出图片
       </Button>
     </div>
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,14 +1,35 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use image::GenericImageView;
+
 #[tauri::command]
 fn greet(name: &str) -> String {
-   format!("Hello, {}!", name)
+    format!("Hello, {}!", name)
+}
+
+#[tauri::command]
+fn image_crop(path: &str) -> String {
+    // 切图操作
+    // Use the open function to load an image from a Path.
+    // `open` returns a `DynamicImage` on success.
+    let img = image::open(path).unwrap();
+
+    // The dimensions method returns the images width and height.
+    println!("dimensions {:?}", img.dimensions());
+
+    // The color method returns the image's `ColorType`.
+    println!("{:?}", img.color());
+
+    // Write the contents of this image to the Writer in PNG format.
+    img.save("test.png").unwrap();
+
+    format!("Hello, {}!", name)
 }
 
 fn main() {
-  tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![greet])
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![greet, image_crop])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
