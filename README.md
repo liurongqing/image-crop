3af301010e5679f363105079ee9e2fe7305288c2
liurongqing <liurongqing@zhihuiqiche.com>
2023-08-01 18:25:26 +0800

    feat: 快速提交

--- README.md
+++ README.md
@@ -3,3 +3,16 @@
 开发中... 暂不可用
 
 主要供于：游戏按需加载时使用
+
+
+## 快速上手
+
+```bash
+# 本地浏览器调试
+npm run dev
+
+# 桌面应用调试
+npm run tauri dev
+```
+
+打开调试快捷键： Command + Option + i 
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,44 @@
 "use client";
+
 import { useEffect } from "react";
 import { invoke } from "@tauri-apps/api/tauri";
+import { InboxOutlined } from "@ant-design/icons";
+import { Button, Card, Form, Input, Progress, Upload } from "antd";
+
+const { Dragger } = Upload;
 
 export default function Home() {
-  useEffect(() => {
-    invoke<string>("greet", { name: "Next.js" })
-      .then(console.log)
-      .catch(console.error);
-  }, []);
-  return <div>123 - 001</div>;
+  return (
+    <div className="p-2">
+      <Dragger>
+        <p className="ant-upload-drag-icon">
+          <InboxOutlined />
+        </p>
+        <p className="ant-upload-text">拖拽或者点击这里上传图片</p>
+        <p className="ant-upload-hint">
+          建议：切图的比例要计算好，每块小图大小最好保持一致。
+        </p>
+      </Dragger>
+
+      <Card title="设置" bordered={false}>
+        <Form>
+          <Form.Item label="宽度">
+            <Input placeholder="255" />
+          </Form.Item>
+          <Form.Item label="高度">
+            <Input placeholder="255" />
+          </Form.Item>
+          <Form.Item label="品质">
+            <Input placeholder="50" />
+          </Form.Item>
+        </Form>
+      </Card>
+
+      <Progress className="mt-4" percent={50} status="active" />
+
+      <Button type="primary" block>
+        导出图片
+      </Button>
+    </div>
+  );
 }
